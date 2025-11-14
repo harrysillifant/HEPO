e3a194024c673ba10415451c1bcbb306e83386a3
Harry <hcs@dhcp-10-29-140-137.dyn.MIT.EDU>
2025-11-14 16:42:35 -0500

    .

--- buffers.py
+++ buffers.py
@@ -1,1 +1,18 @@
-class HEPOBuffer:
+from stable_baselines3.common import RolloutBuffer
+from gymnasium import spaces
+import torch
+from typing import Union
+
+
+class HEPOBuffer(RolloutBuffer):
+    def __init__(
+        self,
+        buffer_size: int,
+        observation_space: spaces.Space,
+        action_space: spaces.Space,
+        device: Union[torch.device, str] = "auto",
+        gae_lambda: float = 1,
+        gamma: float = 0.99,
+        n_envs: int = 1,
+    ):
+        pass
