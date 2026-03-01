ec500f7e17c58e24b4fbda72d30a91a022e61c7b
hacosi <harrysillifant13@icloud.com>
2026-03-01 16:23:06 -0500

    fix time limit

--- run_scripts/long_test.sh
+++ run_scripts/long_test.sh
@@ -5,6 +5,6 @@
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=32
 #SBATCH --mem=64G
-#SBATCH --time=12:00:00
+#SBATCH --time=06:00:00
 
-uv run ppo_hb.py --seed 0 --env_name h1hand-sit_simple-v0 --device cuda --num_envs 32 --name gpu_test
+uv run ppo_hb.py --seed 0 --env_name h1hand-sit_simple-v0 --device cuda --num_envs 32 --name engaging_long_test
